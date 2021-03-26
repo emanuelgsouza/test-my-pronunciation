abe351aabf50efdf7246dd4372744f55b5e976dd
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-03-25 23:14:33 -0300

    feat: move to select page when not exist a challenge

--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>{{ $route.params }}</div>
+  <template v-if="hasChallenge">
+    <div>{{ $route.params }}</div>
+
+    <div>{{ currentChallenge }}</div>
+  </template>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { useStore } from "@/store";
+import { computed, defineComponent, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "ChallengeView",
+
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    const currentChallenge = computed(
+      () => store.state.challenge.currentChallenge
+    );
+
+    const hasChallenge = computed(() => store.getters.hasChallenge);
+
+    onMounted(() => {
+      if (!hasChallenge.value) {
+        router.push({
+          name: "SelectPage",
+        });
+      }
+    });
+
+    return {
+      hasChallenge,
+      currentChallenge,
+    };
+  },
 });
 </script>
