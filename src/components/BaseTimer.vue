01b7246e9b0d88bb2855540a312e7691219fa093
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-03-26 21:30:02 -0300

    feat: create `BaseTimer` component

---
+++ src/components/BaseTimer.vue
@@ -0,0 +1,38 @@
+<template>
+  <span>{{ computedTimer }}</span>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+import { formatTimer } from "@/support";
+
+export default defineComponent({
+  name: "Timer",
+
+  setup() {
+    const timeout = ref(0);
+    const timer = ref(0);
+
+    const computedTimer = computed(() => formatTimer(timer.value));
+
+    const startTimer = () => {
+      stopTimer();
+
+      timeout.value = setInterval(() => {
+        timer.value += 1;
+      }, 1000);
+    };
+
+    const stopTimer = () => {
+      clearInterval(timeout.value);
+    };
+
+    return {
+      timer,
+      computedTimer,
+      startTimer,
+      stopTimer,
+    };
+  },
+});
+</script>
