ffd283d38781daecb6340fc1abb3d7186060871b
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-03-26 21:31:09 -0300

    feat: create initial logic in the Challenge view with recording voice logic

--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -1,29 +1,130 @@
 <template>
   <template v-if="hasChallenge">
-    <div>{{ $route.params }}</div>
+    <p v-if="hasRecorded" class="font-bold text-2xl text-center">
+      Gostaria de verificar seu desempenho?
+    </p>
 
-    <div>{{ currentChallenge }}</div>
+    <p v-else class="font-bold text-2xl text-center">
+      Agora grave sua voz lendo a frase abaixo
+    </p>
+
+    <BaseTimer ref="timer" class="text-2xl mt-16" />
+
+    <p class="text-center my-20">{{ currentChallengeText }}</p>
+
+    <template v-if="!hasRecorded">
+      <BaseButton
+        v-if="!isRecording"
+        label="Gravar voz"
+        @click="startRecording"
+      />
+
+      <BaseButton
+        v-else
+        label="Parar gravação"
+        variant="ghost"
+        @click="finishRecording"
+      />
+    </template>
+
+    <template v-else>
+      <BaseButton
+        class="mb-6"
+        label="Verificar gravação"
+        @click="checkRecording"
+      />
+
+      <!-- <BaseButton
+        class="mb-6"
+        label="Ouvir gravação"
+        variant="secondary"
+        @click="listenRecording"
+      /> -->
+
+      <BaseButton
+        class="mb-6"
+        label="Gravar novamente"
+        variant="ghost"
+        @click="startRecording"
+      />
+    </template>
   </template>
 </template>
 
-<script>
+<script lang="ts">
 import { useStore } from "@/store";
-import { computed, defineComponent, onMounted } from "vue";
+import { computed, defineComponent, onMounted, Ref, ref } from "vue";
 import { useRouter } from "vue-router";
+
+import BaseTimer from "@/components/BaseTimer.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import VoiceRecognition from "@/support/voice-recognization";
 
 export default defineComponent({
   name: "ChallengeView",
+
+  components: { BaseTimer, BaseButton },
 
   setup() {
     const store = useStore();
     const router = useRouter();
 
+    const voiceRecognization = new VoiceRecognition();
+
+    // you can use the component as ref in this case
+    const timer: Ref<typeof BaseTimer> = ref(BaseTimer);
+
+    // vuex challenge module
     const currentChallenge = computed(
       () => store.state.challenge.currentChallenge
     );
 
     const hasChallenge = computed(() => store.getters.hasChallenge);
 
+    const currentChallengeText = computed(() => {
+      return currentChallenge.value.sentence.value;
+    });
+
+    // recording logic
+    const isRecording = ref(false);
+    const hasRecorded = ref(false);
+
+    const startRecording = () => {
+      isRecording.value = true;
+      hasRecorded.value = false;
+
+      // you can access the component instance using .value
+      timer.value.startTimer();
+
+      voiceRecognization
+        .start()
+        .then((transcript) => {
+          console.log(transcript);
+          finishRecording();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const finishRecording = () => {
+      isRecording.value = false;
+      hasRecorded.value = true;
+
+      timer.value.stopTimer();
+
+      voiceRecognization.stop();
+    };
+
+    const listenRecording = () => {
+      console.log("listen");
+    };
+
+    const checkRecording = () => {
+      console.log("check");
+    };
+
+    // hooks
     onMounted(() => {
       if (!hasChallenge.value) {
         router.push({
@@ -33,8 +134,22 @@ export default defineComponent({
     });
 
     return {
+      timer,
+
+      // states
+      isRecording,
+      hasRecorded,
+
+      // computeds
       hasChallenge,
+      currentChallengeText,
       currentChallenge,
+
+      // methods
+      checkRecording,
+      listenRecording,
+      startRecording,
+      finishRecording,
     };
   },
 });
