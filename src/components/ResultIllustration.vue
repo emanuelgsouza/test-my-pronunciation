41d1dde9345f70de95363a910e7228c709f46a5b
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-03-30 22:09:42 -0300

    feat: continue implementing the Result page

---
+++ src/components/ResultIllustration.vue
@@ -0,0 +1,41 @@
+<template>
+  <img src="@/assets/illustrations/sad.svg" alt="" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+import { isAverage, isGood } from "@/support";
+
+type ComponentProps = {
+  rate: number;
+};
+
+export default defineComponent({
+  name: "ResultIllustration",
+
+  rate: {
+    type: Number,
+    default: 0,
+  },
+
+  // TODO: make this more intelligent
+  setup(props: ComponentProps) {
+    const illustrationPath = computed(() => {
+      if (isGood(props.rate)) {
+        return "../assets/illustrations/congrats.svg";
+      }
+
+      if (isAverage(props.rate)) {
+        return "../assets/illustrations/almost-there.svg";
+      }
+
+      return "../assets/illustrations/sad.svg";
+    });
+
+    return {
+      illustrationPath,
+    };
+  },
+});
+</script>
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,27 +1,100 @@
 <template>
-  <div>Resultado</div>
+  <p class="font-bold text-xl">{{ computedTitle }}</p>
 
-  <div>{{ currentChallenge }}</div>
+  <ResultIllustration class="my-10 mx-auto" :rate="computedRate" />
+
+  <p class="text-2xl mb-10">{{ computedPercent }}% de acerto!</p>
+
+  <p class="font-bold text-xl">Do texto que propomos...</p>
+
+  <p class="my-10">{{ originalText }}</p>
+
+  <p class="font-bold text-xl">Você falou....</p>
+
+  <p class="my-10">{{ transcript }}</p>
+
+  <BaseButton
+    class="mb-6"
+    label="Tentar outro desafio!"
+    @click="tryAnotherChallenge"
+  />
+
+  <BaseButton label="Gravar novamente" variant="ghost" @click="tryAgain" />
 </template>
 
 <script>
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
+
+import ResultIllustration from "@/components/ResultIllustration.vue";
+import BaseButton from "@/components/BaseButton.vue";
 
 import { useStore } from "@/store";
+import { calculateRate, isGood, isAverage } from "@/support";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "ResultPage",
 
+  components: {
+    BaseButton,
+    ResultIllustration,
+  },
+
+  // TODO: implement logic to get result from the stores's challenges object
   setup() {
     // implement logic to get by using the uuid
     const store = useStore();
+    const router = useRouter();
 
     const currentChallenge = computed(
       () => store.state.challenge.currentChallenge
     );
 
+    const originalText = ref(currentChallenge.value.sentence.value);
+    const transcript = ref(currentChallenge.value.result.transcript);
+
+    const computedRate = calculateRate(originalText, transcript);
+
+    const computedPercent = computed(() => computedRate * 100);
+
+    const computedTitle = computed(() => {
+      if (isGood(computedRate)) {
+        return "Parabéns!";
+      }
+
+      if (isAverage(computedRate)) {
+        return "Quase lá!";
+      }
+
+      return "Não foi dessa vez!";
+    });
+
+    const tryAnotherChallenge = () => {
+      router.push({
+        name: "SelectPage",
+      });
+    };
+
+    const tryAgain = () => {
+      router.push({
+        name: "ChallengePage",
+        params: {
+          uuid: currentChallenge.value.uuid,
+        },
+      });
+    };
+
     return {
+      originalText,
+      transcript,
+
+      computedRate,
+      computedTitle,
+      computedPercent,
       currentChallenge,
+
+      tryAgain,
+      tryAnotherChallenge,
     };
   },
 });
