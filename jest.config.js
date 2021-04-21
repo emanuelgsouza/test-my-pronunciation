0d91ea88b4e77e172e493c44c9692d7a23502f62
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-04-21 12:39:44 -0300

    chore: use `jest-transform-stub` to stub svg files

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
+  moduleNameMapper: {
+    "\\.(svg)$": "jest-transform-stub",
+  },
   setupFilesAfterEnv: ["./jest.setup.js"],
 };
