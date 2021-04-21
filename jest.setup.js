ae6e2c8ee0f3130a42d9b101dd1ec7176cbad616
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-04-21 12:37:49 -0300

    chore: mock SpeechRecognition api to fix runtime errors on tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 // In your own jest-setup.js (or any other name)
 import "@testing-library/jest-dom";
+
+import { SpeechRecognitionMock } from "speech-recognition-mock";
+
+Object.defineProperty(window, "SpeechRecognition", {
+  writable: true,
+  value: SpeechRecognitionMock,
+});
